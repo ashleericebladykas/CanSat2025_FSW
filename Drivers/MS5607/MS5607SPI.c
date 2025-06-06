3f30ccfbf8c1282c701f793c9e8d45ccaaa44024
PotatoCorn001 <sarahandaunty@gmail.com>
2025-06-06 19:02:06 -0400

    Compute state based on altitude history.

--- Drivers/MS5607/MS5607SPI.c
+++ Drivers/MS5607/MS5607SPI.c
@@ -328,68 +328,91 @@ void MS5607SetPressureOSR(MS5607OSRFactors pOSR)
   Pressure_OSR = pOSR;
 }
 
+/*
+  AltitudeCalculations.c START HERE
+*/
 float const lower_altitude_threshold = 5.0;
+
+// Index is by seconds ago the value was calculated
+float altitude_history[] = {0, 0, 0};
+
 float max_altitude = 0.0;
-
-float apogee_base_ratio = 0.75;
+float apogee_base_ratio = 0.75; 
 float apogee_difference_ratio = 0.00;
+float const apogee_offset_height = 20.00;
 
-float const alt_offset_height = 20.00;
-float const accel_tolerance = 0.1;
+float calibrated_atitude = 0.00;
 
-float calibrated_altitude = 0.00;
+/*
+Units
+Pressure - mbars - double
+Temperature - Celcius - double
+*/
 
-float calculateAltitude(double pressure, int calibrating) {
-	double pressure_mb = 33.8639 * (0.2953 * pressure);
-	float h_meter = 0.3048 * (1 - pow((pressure_mb / 1013.25), 0.190284)) * 145366.54;
-	if (calibrating == 1)
-	{
-		// Absolute Altitude of the ground station
-	    calibrated_altitude = h_meter;
-	    // Relative Altitude of GCS
-	    return 0;
-	}
-	else
-	{
-	      // Relative Altitude of CanSat
-	   return h_meter - calibrated_altitude;
-	}
-	//return 100.0;
+// https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf
+// The altitude equation is for absolute altitude.
+// calibraing : 1 = True, 0 = False
+float caluclateAltitude(double pressure, int calibrating){
+    float pressure_mb = 33.8639 * (0.2953 * pressure);
+	float h_meter = 0.3048 * (1 - pow((pressure_mb / 1013.25), 0.190284)) * 145366.54;er = 0.3048*((1 - pow((pressure/1013.25), 0.190284))*145366.54);
+    if (calibrating == 1){
+        // Absolute Altitude of the ground station
+        calibrated_atitude = h_meter;
+        // Relative Altitude of GCS
+        return 0;
+    }
+    else{
+        // Relative Altitude
+        altitude_history[2] = altitude_history[1];
+        altitude_history[1] = altitude_history[0];
+        altitude_history[0] = h_meter - calibrated_atitude;
+        return altitude_history[0];
+    }
 }
 
-// Rather than using a history of altitudes, we figure out when acceleration matches the acceleration of gravity.
-// Includes a tolerance to ensure apogee state is reached.
-void determine_state(double altitude, ICM42688P_AccelData data)
-{
-  if (altitude > lower_altitude_threshold && strcmp(global_mission_data.STATE, "LAUNCH_PAD"))
-  {
-    char _state[] = "ASCENT";
-    memcpy(global_mission_data.STATE, _state, sizeof(_state));
-  }
-  else if (altitude < lower_altitude_threshold && strcmp(global_mission_data.STATE, "PROBE_RELEASE"))
-  {
-    char _state[] = "LANDED";
-    memcpy(global_mission_data.STATE, _state, sizeof(_state));
-  }
-  else if (data.accel_z >= (1 - accel_tolerance))
-  {
-    if (max_altitude == 0)
+// Idea is to calculateAltitude then immediately call this function
+// to detemrine state.
+void determineState(double altitude){
+    // Set to ASCENT.
+    if(altitude > lower_altitude_threshold 
+        && strcmp(global_mission_data.STATE, "LAUNCH_PAD") == 0)
     {
-      max_altitude = altitude;
-      apogee_difference_ratio = alt_offset_height / max_altitude;
-      char _state[] = "APOGEE";
-      memcpy(global_mission_data.STATE, _state, sizeof(_state));
+        char _state[] = "ASCENT";
+        memcpy(global_mission_data.STATE, _state, sizeof(_state));
     }
-    else if (max_altitude * (apogee_base_ratio + apogee_difference_ratio) > altitude)
+    // Set to APOGEE.
+    else if (altitude_history[2] > altitude_history[1] && altitude_history[2] > altitude_history[0] 
+        && strcmp(global_mission_data.STATE, "ASCENT") == 0)
+    {   
+        // Calculate where probe should be released at.
+        max_altitude = altitude;
+        apogee_difference_ratio = apogee_offset_height / max_altitude;
+
+        // Set flag to start heating up the resistor.
+        mec_wire_enable = 1;
+
+        char _state[] = "APOGEE";
+        memcpy(global_mission_data.STATE, _state, sizeof(_state));
+    }
+    // Set to DESCENT
+    else if(strcmp(global_mission_data.STATE, "APOGEE") == 0
+        && altitude > max_altitude*(apogee_base_ratio + apogee_difference_ratio))
     {
-      char _state[] = "PROBE_RELEASE";
-      memcpy(global_mission_data.STATE, _state, sizeof(_state));
-      // DO ACTUATOR STUFF
+        char _state[] = "DESCENT";
+        memcpy(global_mission_data.STATE, _state, sizeof(_state));
     }
-    else
+    // Set to PROBE_RELEASE
+    else if((strcmp(global_mission_data.STATE, "DESCENT") == 0 || strcmp(global_mission_data.STATE, "APOGEE"))
+        && altitude < max_altitude*(apogee_base_ratio + apogee_difference_ratio))
     {
-      char _state[] = "DESCENT";
-      memcpy(global_mission_data.STATE, _state, sizeof(_state));
+        char _state[] = "PROBE_RELEASE";
+        memcpy(global_mission_data.STATE, _state, sizeof(_state));
     }
-  }
+    // Set to LANDED
+    else if(strcmp(global_mission_data.STATE, "PROBE_RELEASE") == 0
+        && altitude < lower_altitude_threshold)
+    {
+        char _state[] = "LANDED";
+        memcpy(global_mission_data.STATE, _state, sizeof(_state));
+    }
 }
